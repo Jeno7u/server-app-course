106e47d05a1638121c948147a28b52b1e3f81057
Mironov Boris <lboris201@yandex.ru>
2026-02-14 15:08:05 +0300

    Task 1.2 Now root route returns html page

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  router := gin.Default()
-  router.GET("/", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "Добро пожаловать в моё приложение Gin!",
-    })
-  })
-  router.Run() // listens on 0.0.0.0:8080 by default
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		data, err := os.ReadFile("internal/src/index.html")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.Data(http.StatusOK, "text/html; charset=utf-8", data)
+	})
+
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
